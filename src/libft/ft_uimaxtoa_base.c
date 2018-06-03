72bc62e9f5b701b3305d2fb563f405b2b9af226b
StevenSteinbeck <stevensteinbeck@yahoo.com>
2018-06-03 13:00:04 -0700

    Add files via upload

--- src/libft/ft_uimaxtoa_base.c
+++ src/libft/ft_uimaxtoa_base.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 15:41:23 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/03 12:49:16 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/03 12:54:48 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 char	*ft_uimaxtoa_base(uintmax_t value, int8_t base, const char *str)
 {
-	uintmax_t	i;
-	size_t		size;
-	char		*ret;
+	t_uimax	head;
 
-	i = value;
-	size = 1;
-	while ((i /= base))
-		size++;
-	ret = (char *)malloc(sizeof(char) * (size + 1));
-	ret[size] = '\0';
-	i = value;
-	ret[--size] = str[i % base];
-	while ((i /= base))
-		ret[--size] = str[i % base];
-	return (ret);
+	head.i_val = value;
+	if (head.i_val != value)
+		return (NULL);
+	head.size = 1;
+	while ((head.i_val /= base))
+		head.size++;
+	head.retrn = (char *)malloc(sizeof(char) * (head.size + 1));
+	head.retrn[head.size] = '\0';
+	head.i_val = value;
+	head.retrn[--head.size] = str[head.i_val % base];
+	while ((head.i_val /= base))
+		head.retrn[--head.size] = str[head.i_val % base];
+	return (head.retrn);
 }
