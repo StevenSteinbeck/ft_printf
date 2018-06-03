859317672142794d7f170bf8b7c1587044dfb7d6
StevenSteinbeck <stevensteinbeck@yahoo.com>
2018-06-03 12:44:48 -0700

    Add files via upload

--- src/libft/ft_uimaxtoa_base.c
+++ src/libft/ft_uimaxtoa_base.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 15:41:23 by stestein          #+#    #+#             */
-/*   Updated: 2018/05/31 17:00:58 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/03 12:44:09 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,19 @@
 
 char	*ft_uimaxtoa_base(uintmax_t value, int8_t base, const char *str)
 {
-	t_uimax	*head;
+	t_uimax	head;
 
-	head = malloc(sizeof(t_uimax));
-	head->i = value;
-	if (head->i != value)
-	{
-		free(head);
+	head.i = value;
+	if (head.i != value)
 		return (NULL);
-	}
-	head->size = 1;
-	while ((head->i /= base))
-		head->size++;
-	head->ret = (char *)ft_memalloc(sizeof(char) * (head->size + 1));
-	head->ret[head->size] = '\0';
-	head->i = value;
-	head->ret[--head->size] = str[head->i % base];
-	while ((head->i /= base))
-		head->ret[--head->size] = str[head->i % base];
-	free(head);
-	return (head->ret);
+	head.size = 1;
+	while ((head.i /= base))
+		head.size++;
+	head.ret = (char *)ft_memalloc(sizeof(char) * (head.size + 1));
+	head.ret[head.size] = '\0';
+	head.i = value;
+	head.ret[--head.size] = str[head.i % base];
+	while ((head.i /= base))
+		head.ret[--head.size] = str[head.i % base];
+	return (head.ret);
 }
