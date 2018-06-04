a74af13c4f18fc6844c4e7c099d4bfa0c32ea645
Steven Steinbeck <stestein@e1z3r3p18.42.us.org>
2018-06-03 21:35:59 -0700

    ey

--- src/ft_num_conv.c
+++ src/ft_num_conv.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/29 18:09:55 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/03 21:35:04 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/03 21:35:55 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ intmax_t	ft_int_len(char length, va_list ap)
 
 	top = malloc(sizeof(t_num));
 	head.l = length;
-	top->l = head.l
+	top->l = head.l;
 	free(top);
 	if (head.l == hh)
 		FLAG_HH;
