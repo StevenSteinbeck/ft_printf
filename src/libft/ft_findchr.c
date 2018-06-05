684f9fcecc1c5f0a9520b11d06f432206cc180e3
Steven Steinbeck <stestein@e1z3r2p23.42.us.org>
2018-06-05 16:08:24 -0700

    ey

--- src/libft/ft_findchr.c
+++ src/libft/ft_findchr.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 13:59:04 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/03 13:59:05 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/05 16:01:17 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 size_t		ft_findchr(const char *s, int c)
 {
 	int				i;
+	unsigned int	p;
 	unsigned char	a;
 
 	a = (unsigned char)c;
 	i = 0;
+	p = 0;
+	if (p != 0)
+		return (0);
 	while (s[i] != '\0')
 	{
-		if (s[i] == a)
+		if (s[i] == a && p == 0)
 			return (i);
 		i++;
 	}
--- src/libft/ft_imaxtoa.c
+++ src/libft/ft_imaxtoa.c
@@ -6,29 +6,31 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/03 13:59:16 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/03 13:59:17 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/05 16:07:44 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
+#include "libftprintf.h"
 
 char	*ft_imaxtoa(intmax_t value)
 {
-	uintmax_t	i;
-	intmax_t	size;
+	t_num		*top;
 	char		*ret;
 
-	i = (value < 0) ? -value : value;
-	size = 1 + (value < 0);
-	while ((i /= 10))
-		size++;
-	ret = (char *)malloc(sizeof(char) * (size + 1));
-	ret[size] = '\0';
-	i = (value < 0) ? -value : value;
-	ret[--size] = "0123456789"[i % 10];
-	while ((i /= 10))
-		ret[--size] = "0123456789"[i % 10];
+	top = malloc(sizeof(t_num));
+	top->hex = (value < 0) ? -value : value;
+	top->val = 1 + (value < 0);
+	while ((top->hex /= 10))
+		top->val++;
+	ret = (char *)malloc(sizeof(char) * (top->val + 1));
+	ret[top->val] = '\0';
+	top->hex = (value < 0) ? -value : value;
+	ret[--top->val] = "0123456789"[top->hex % 10];
+	while ((top->hex /= 10))
+		ret[--top->val] = "0123456789"[top->hex % 10];
 	if (value < 0)
-		ret[--size] = '-';
+		ret[--top->val] = '-';
+	free(top);
 	return (ret);
 }
