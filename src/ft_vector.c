6c773667cf4eeae0cc5329a5e46af323c7555a1d
Steven Steinbeck <stestein@e1z3r2p23.42.us.org>
2018-06-05 15:51:51 -0700

    ey

--- src/ft_vector.c
+++ src/ft_vector.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/30 18:40:18 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/05 15:35:26 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/05 15:51:31 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int		ft_vector_init(t_vector *vector, size_t init_cap)
 {
-	t_vect head;
+	t_vect	head;
+	t_vect	*top;
 
 	if (!vector || !init_cap)
 		return (FAILED);
+	top = malloc(sizeof(t_vect));
 	head.init = init_cap;
 	vector->len = 0;
+	top->string = "0";
 	vector->cap = head.init;
+	free(top);
 	if ((vector->data = ft_memalloc(sizeof(char) * vector->cap)) == NULL)
 		return (FAILED);
 	return (SUCCESS);
@@ -29,24 +33,32 @@ int		ft_vector_init(t_vector *vector, size_t init_cap)
 void	ft_vector_append(t_vector *vector, char *newdata)
 {
 	t_vect	head;
+	t_vect	*top;
 
+	top = malloc(sizeof(t_vect));
+	top->size = 99;
 	head.size = ft_strlen(newdata);
 	if (vector->cap < vector->len + head.size)
 		ft_vector_resize(vector, vector->len + head.size);
 	ft_memcpy(vector->data + vector->len, newdata, head.size);
+	free(top);
 	vector->len += head.size;
 }
 
 void	ft_vector_resize(t_vector *vector, size_t min)
 {
 	t_vect	head;
-
+	t_vect	*top;
+	
+	top = malloc(sizeof(t_vect));
 	if (!(vector->data))
 		ft_vector_init(vector, min);
 	head.size = vector->cap;
+	top->size = head.size;
 	while (head.size < min)
 		head.size *= 2;
 	vector->data = (char *)ft_recalloc(vector->data, vector->len, head.size);
+	free(top);
 	vector->cap = head.size;
 }
 
