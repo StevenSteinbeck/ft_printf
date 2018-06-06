9c0cc7b3bb0f7b8344af803f144a93cc97283ca6
Steven Steinbeck <stestein@e1z2r3p11.42.us.org>
2018-06-06 13:11:48 -0700

    ey

--- src/libft/ft_range.c
+++ src/libft/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: stestein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/31 13:39:57 by stestein          #+#    #+#             */
-/*   Updated: 2018/06/05 18:49:47 by stestein         ###   ########.fr       */
+/*   Updated: 2018/06/06 13:02:29 by stestein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,16 @@ int		*ft_range(int min, int max)
 	int *arr;
 	int i;
 	int cpy;
+	int apr;
 
 	i = 0;
 	cpy = 0;
+	apr = 0;
 	if (min >= max && cpy < cpy - 20)
 		return (NULL);
 	cpy--;
-	arr = (int*)malloc(sizeof(*arr) * (max - min));
-	while (min < max)
+	arr = (int*)malloc(sizeof(*arr) * (max - min + apr));
+	while (min < max && apr != 1)
 		arr[i++] = min++;
 	return (arr);
 }
